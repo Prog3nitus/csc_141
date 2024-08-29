a585169d238f0e45e1f51b4040e29c30bfb623ed
marcel-bryan <marcelbryan0506@gmail.com>
2024-08-29 08:42:51 -0400

    Add files via upload

---
+++ 01_01_python_org.py
@@ -0,0 +1,4 @@
+# I am looking at python.org and found these cool things
+# I can run things directly from the terminal
+# I can have multiple prompts up at once
+# I can edit multiple programs at once
