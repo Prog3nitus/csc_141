38ad156844bee92e6db53b68547eb6aa91efd7ab
marcel-bryan <marcelbryan0506@gmail.com>
2024-09-10 09:11:59 -0400

    Add files via upload

---
+++ 01_03_infinite_skills.py
@@ -0,0 +1,7 @@
+# I want to be able to learn several skills with this class like learning how to create games
+# and potentially making my own simple rpg or just a simple game itself.
+# I wann learn to create my own AI that would help me with the D&D campaign I want to make and learn
+# how to make websites and how to make a fuctional and working game. I also wanna learn how to create 
+# sprites and use different engines and languages and other softwares and applications and get better
+# with things like visual studios. I also wanna create my own magic system via an AI software and use
+# AI alot more in things I use and create because of the plethra of ways it can be used.
